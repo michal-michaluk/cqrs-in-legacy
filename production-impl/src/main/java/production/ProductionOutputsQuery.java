3cbb9add169fd97941542938308ffbb0eb6f661d
Michał Michaluk <michaluk.michal@gmail.com>
2019-12-04 11:41:44 +0100

    immutable Read Model free of entities

--- production-impl/src/main/java/production/ProductionOutputsQuery.java
+++ production-impl/src/main/java/production/ProductionOutputsQuery.java
@@ -1,14 +1,22 @@
 package production;
 
 import dao.ProductionDao;
+import entities.ProductionEntity;
 import shortages.ProductionOutputs;
 
 import java.time.LocalDate;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ProductionOutputsQuery {
     private ProductionDao productionDao;
 
     public ProductionOutputs readOutputs(String productRefNo, LocalDate today) {
-        return new ProductionOutputs(productionDao.findFromTime(productRefNo, today.atStartOfDay()));
+        Map<LocalDate, Long> outputs = productionDao.findFromTime(productRefNo, today.atStartOfDay()).stream()
+                .collect(Collectors.groupingBy(
+                        production -> production.getStart().toLocalDate(),
+                        Collectors.summingLong(ProductionEntity::getOutput)
+                ));
+        return new ProductionOutputs(productRefNo, outputs);
     }
 }
