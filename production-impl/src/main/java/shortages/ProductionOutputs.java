ed68f251159f215bc16dccfd12ec50ccbdc2134f
Michał Michaluk <michaluk.michal@gmail.com>
2019-12-04 11:23:40 +0100

    Intorduction of read model api

---
+++ production-impl/src/main/java/shortages/ProductionOutputs.java
@@ -0,0 +1,36 @@
+package shortages;
+
+import entities.ProductionEntity;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProductionOutputs {
+
+    private final Map<LocalDate, ProductionEntity> outputs;
+    private String productRefNo;
+
+    public ProductionOutputs(List<ProductionEntity> productions) {
+        productRefNo = null;
+        outputs = new HashMap<>();
+        for (ProductionEntity production : productions) {
+            outputs.put(production.getStart().toLocalDate(), production);
+            productRefNo = production.getForm().getRefNo();
+        }
+    }
+
+    public long getOutput(LocalDate day) {
+        ProductionEntity production = outputs.get(day);
+        if (production != null) {
+            return production.getOutput();
+        } else {
+            return 0;
+        }
+    }
+
+    public String getProductRefNo() {
+        return productRefNo;
+    }
+}
